5f8ded75b48ca9a52fcab60c8abbd7670ffc6d37
gokulkrishnamohn <gokulkrishnamohn@gmail.com>
2025-03-27 05:34:47 -0400

    feat: added ageGroup controller and service

--- src/main/java/com/tinysteps/tinysteps/controller/AgeGroupController.java
+++ src/main/java/com/tinysteps/tinysteps/controller/AgeGroupController.java
@@ -1,17 +1,21 @@
 package com.tinysteps.tinysteps.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tinysteps.tinysteps.model.AgeGroupModel;
 import com.tinysteps.tinysteps.service.AgeGroupService;
-
 
 @RestController
 @RequestMapping("/api/agegroup")
@@ -28,5 +32,13 @@ public class AgeGroupController {
     @PostMapping("/add")
     public String addAgeGroup(@RequestBody AgeGroupModel ageGroup) {        
         return ageGroupService.addAgeGroup(ageGroup);
+    }
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Map<String, Object>> deleteAgeGroup(@PathVariable Long id) {
+        return ageGroupService.deleteAgeGroup(id);
+    }
+    @PutMapping("edit/{id}")
+    public String editAgeGroup(@RequestBody AgeGroupModel ageGroupModel) {
+        return ageGroupService.editAgeGroup(ageGroupModel);
     }
 }
--- src/main/java/com/tinysteps/tinysteps/service/AgeGroupService.java
+++ src/main/java/com/tinysteps/tinysteps/service/AgeGroupService.java
@@ -1,7 +1,12 @@
 package com.tinysteps.tinysteps.service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.tinysteps.tinysteps.model.AgeGroupModel;
@@ -28,6 +33,33 @@ public class AgeGroupService {
         ageGroupRepository.save(ageGroup);
 
         return "Age group added successfully!";
+    }
+    public ResponseEntity<Map<String, Object>> deleteAgeGroup(Long id) {
+        Optional<AgeGroupModel> existingAgeGroup = ageGroupRepository.findById(id);
+
+        if (!existingAgeGroup.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(Map.of("message", "AgeGroup is missing or invalid", "data", Collections.emptyMap()));
+
+        }
+
+        ageGroupRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(Map.of("message", "AgeGroup deleted successfully", "data", Collections.emptyMap()));
+
+    }
+    public String editAgeGroup(AgeGroupModel ageGroupModel) {
+        Long id = ageGroupModel.getId();
+        Optional<AgeGroupModel> existingAgeGroup = ageGroupRepository.findById(id);
+        System.out.print(existingAgeGroup);
+
+        if (!existingAgeGroup.isPresent()) {
+            return "No ageGroup exists with the given ID!";
+        }
+
+        ageGroupRepository.save(ageGroupModel);
+
+        return "AgeGroup updated successfully!";
     }
 
 }
