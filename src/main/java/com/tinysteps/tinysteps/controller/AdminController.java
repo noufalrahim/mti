70c2ece57c8abae714605117ce1b430b8e99dc4e
gokulkrishnamohn <gokulkrishnamohn@gmail.com>
2025-03-27 05:27:59 -0400

    feat: added admin controller and service

--- src/main/java/com/tinysteps/tinysteps/controller/AdminController.java
+++ src/main/java/com/tinysteps/tinysteps/controller/AdminController.java
@@ -1,10 +1,15 @@
 package com.tinysteps.tinysteps.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +32,14 @@ public class AdminController {
     @PostMapping("/add")
     public String addAdmin(@RequestBody AdminModel adminModel) {
         return adminService.addAdmin(adminModel.getName());
+    }
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Map<String, Object>> deleteAdmin(@PathVariable String id) {
+        Long adminId = Long.valueOf(id);
+        return adminService.deleteAdmin(adminId);
+    }
+    @PutMapping("edit/{id}")
+    public String editAdmin(@RequestBody AdminModel adminModel) {
+        return adminService.editAdmin(adminModel);
     }
 }
--- src/main/java/com/tinysteps/tinysteps/service/AdminService.java
+++ src/main/java/com/tinysteps/tinysteps/service/AdminService.java
@@ -1,9 +1,13 @@
 package com.tinysteps.tinysteps.service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.tinysteps.tinysteps.model.AdminModel;
@@ -35,5 +39,32 @@ public class AdminService {
         adminRepository.save(newAdmin);
 
         return "Admin added successfully!";
+    }
+    public ResponseEntity<Map<String, Object>> deleteAdmin(Long id) {
+        Optional<AdminModel> existingAdmin = adminRepository.findById(id);
+
+        if (!existingAdmin.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(Map.of("message", "Admin is missing or invalid", "data", Collections.emptyMap()));
+
+        }
+
+        adminRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(Map.of("message", "Admin deleted successfully", "data", Collections.emptyMap()));
+
+    }
+    public String editAdmin(AdminModel adminModel) {
+        Long id = adminModel.getId();
+        Optional<AdminModel> existingAdmin = adminRepository.findById(id);
+        System.out.print(existingAdmin);
+
+        if (!existingAdmin.isPresent()) {
+            return "No admin exists with the given ID!";
+        }
+
+        adminRepository.save(adminModel);
+
+        return "Admin updated successfully!";
     }
 }
